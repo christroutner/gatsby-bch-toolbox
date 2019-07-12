f114d8743e69f0a1c97fd0a86617730113438c53
Chris Troutner <chris.troutner@gmail.com>
2019-07-11 20:29:49 -0700

    Adding link to tor site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,47 @@
 
 import React from 'react'
 
-const Footer = (props) => (
-    <footer id="footer">
-        <div className="inner">
-            <ul className="icons">
-                <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
-            </ul>
-            <ul className="copyright">
-                <li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
-            </ul>
-        </div>
-    </footer>
+const Footer = props => (
+  <footer id="footer">
+    <p>
+      This site is also available over Tor: <a href="http://abuq3kgfrzlvrdb4.onion/">abuq3kgfrzlvrdb4.onion</a>
+    </p>
+    <div className="inner">
+      <ul className="icons">
+        <li>
+          <a href="#" className="icon alt fa-twitter">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-facebook">
+            <span className="label">Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-instagram">
+            <span className="label">Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-github">
+            <span className="label">GitHub</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon alt fa-linkedin">
+            <span className="label">LinkedIn</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="copyright">
+        <li>&copy; Untitled</li>
+        <li>
+          Design: <a href="https://html5up.net">HTML5 UP</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
 )
 
 export default Footer
