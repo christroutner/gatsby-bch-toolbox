35917e41ff9a5b5da32eae4c971b8be34947c931
Chris Troutner <chris.troutner@gmail.com>
2019-07-30 16:46:41 -0700

    Updated toolkit-info

--- src/pages/toolkit-info.js
+++ src/pages/toolkit-info.js
@@ -6,12 +6,37 @@ import Layout from '../components/layout'
 
 import harddrive from '../assets/images/hard-drive.jpg'
 
+const directoryStructure = `
+├── full-nodes
+│   └── mainnet
+│   └── testnet
+├── indexers
+│   ├── insight # Insight API v3 used by rest.bitcoin.com
+│   │   ├── mainnet
+│   │   ├── testnet
+│   ├── blockbook
+│   │   ├── mainnet
+│   │   ├── testnet
+│   ├── bitcore
+│   │   ├── mainnet
+│   │   ├── testnet
+│   ├── slpdb
+│   │   ├── mainnet
+│   │   ├── testnet
+├── rest-apis
+│   ├── bch-api
+│   ├── docker-rest.bitcoin.com
+├── libraries
+│   ├── bch-js
+│   ├── bitbox-sdk
+`
+
 class ToolkitInfo extends React.Component {
   render() {
     return (
       <Layout>
         <Helmet>
-          <title>Buy Trout's Bitcoin Cash JavaScript Toolkit</title>
+          <title>Info | Trout's Bitcoin Cash JavaScript Toolkit</title>
           <meta
             name="description"
             content="Buy Trout's Bitcoin Cash JavaScript Toolkit"
@@ -22,7 +47,7 @@ class ToolkitInfo extends React.Component {
           <section id="one">
             <div className="inner">
               <header className="major">
-                <h1>Buy the Toolkit</h1>
+                <h1>About the Toolkit</h1>
               </header>
 
               <div className="grid-wrapper">
@@ -48,32 +73,45 @@ class ToolkitInfo extends React.Component {
                     for software to read this format if you are using those
                     operating systems.
                   </p>
-                  <p>
-                    <b>Price: $200</b> in Bitcoin Cash (BCH).
-                  </p>
-                  <button onClick={this.buyButton}>Buy Now</button>
+                  <center>
+                    <p>
+                      <b>Price: $200</b> in Bitcoin Cash (BCH).
+                    </p>
+
+                    <button className="button" onClick={this.buyButton}>
+                      Buy Now
+                    </button>
+                  </center>
                   <br />
                   <br />
                 </div>
                 <div className="col-1" />
               </div>
 
-              <div className="grid-wrapper">
+              <div
+                className="grid-wrapper"
+                style={{ backgroundColor: '#375182', padding: '30px' }}
+              >
                 <div className="col-6">
                   <h2>Navigating the Stack</h2>
                   <p>
-                    The video discusses the two types of software stacks that
+                    This video discusses the two types of software stacks that
                     can be constructed with the data in the Toolkit.
                   </p>
                   <ul>
                     <li>
-                      A replication of <a href="https://rest.bitcoin.com" target="_blank">
-                      rest.bitcoin.com</a>. (<a href="#">See Diagram</a>)
+                      A replication of{' '}
+                      <a href="https://rest.bitcoin.com" target="_blank">
+                        rest.bitcoin.com
+                      </a>
+                      . (<a href="/rest-stack">See Diagram</a>)
                     </li>
                     <li>
-                      <a href="https://github.com/christroutner/bch-api">bch-api</a> an
-                      alternative implementation of rest.bitcoin.com.
-                      (<a href="#">See Diagram</a>)
+                      <a href="https://github.com/christroutner/bch-api">
+                        bch-api
+                      </a>{' '}
+                      an alternative implementation of rest.bitcoin.com. (
+                      <a href="/bch-api-stack">See Diagram</a>)
                     </li>
                   </ul>
                 </div>
@@ -88,6 +126,88 @@ class ToolkitInfo extends React.Component {
                       allowFullScreen
                     />
                   </div>
+                </div>
+              </div>
+
+              <br />
+
+              <div className="grid-wrapper">
+                <div className="col-6">
+                  <h2>Directory Structure</h2>
+                  <p>
+                    The hard drive you recieve will have the following directory
+                    structure. Each directory will contain:
+                  </p>
+                  <ul>
+                    <li>A zip file of synced data.</li>
+                    <li>
+                      A copy of the repository used to create a Docker container
+                      capable of reading the data in the zip file.
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-6">
+                  <pre style={{ whiteSpace: 'pre-wrap' }}>
+                    {directoryStructure}
+                  </pre>
+                </div>
+              </div>
+
+              <br />
+
+              <div
+                className="grid-wrapper"
+                style={{ backgroundColor: '#375182', padding: '30px' }}
+              >
+                <div className="col-6">
+                  <h2>Setting up the Software</h2>
+                  <p>
+                    This video discusses the basic workflow of starting the
+                    software applications in the Toolkit for setting up an
+                    application stack. Start at the bottom, and run the
+                    applications from bottom to top, in this order:
+                  </p>
+
+                  <ol>
+                    <li>Full node</li>
+                    <li>Indexers</li>
+                    <li>REST API</li>
+                  </ol>
+
+                  <p>
+                    The general workflow for each layer of the stack is the
+                    same:
+                  </p>
+
+                  <ol>
+                    <li>Unzip the data.</li>
+                    <li>Download or build the Docker container.</li>
+                    <li>Run the Docker container.</li>
+                  </ol>
+                </div>
+
+                <div className="col-6">
+                  <div className="youtube-responsive-container">
+                    <iframe
+                      title="Bitcoin Cash JavaScript Toolbox"
+                      src="https://www.youtube.com/embed/NpEaa2P7qZI"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <br />
+
+              <div className="grid-wrapper">
+                <div className="col-12">
+                  <center>
+                    <button className="button" onClick={this.buyButton}>
+                      Buy Now
+                    </button>
+                  </center>
                 </div>
               </div>
             </div>
